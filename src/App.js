2330ca76b619e9ca4ee0bc00647501d0fcccf17a
lmachens <leon.machens@googlemail.com>
2019-10-07 14:49:11 +0200

    add basic layout

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import GlobalStyles from "./GlobalStyles";
+import styled from "styled-components";
+import Logo from "./components/Logo";
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+const Navigation = styled.div`
+  width: 300px;
+  background-color: #414958;
+  text-align: center;
+  padding: 10px 0px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Aside = styled.aside`
+  flex: 1;
+`;
+
+const Main = styled.main`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Hr = styled.hr`
+  height: 1px;
+  width: 100%;
+  background-color: #5b6270;
+  border: none;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <GlobalStyles />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Navigation>
+        <Logo />
+        <Hr />
+        <Aside>Aside</Aside>
+        <Hr />
+        Built with ❤️
+      </Navigation>
+      <Main>Main</Main>
+    </Container>
   );
 }
 
